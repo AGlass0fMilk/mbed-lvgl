7a08a549ed4b14a81d0425bbb8594407c2716ea0
aglass0fmilk <donbeckstein@live.com>
2018-07-16 11:37:46 -0400

    Added in non-functional files for stm32 development

---
+++ drivers/HX8357D/HX8357D.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file HX8357D.cpp
+ * @brief Brief Description
+ * 
+ * Detailed Description
+ *
+ * Link to [markdown like this](@ref PageTag)
+ * Make sure you tag the markdown page like this:
+ * # Page title {#PageTag}
+ * 
+ * <a href='MyPDF.pdf'> Link to PDF documents like this</a>
+ * If you add document files, make sure to add them into a directory inside a "docs" folder
+ * And then run hud-devices/tools/copy-dox-files.py 
+ *
+ * To use images, make sure they're in an "images" folder and follow the doxygen user manual to add images.
+ * You must run copy-dox-files.py after adding images as well.
+ *
+ * @copyright Copyright &copy; 2018 Heads Up Display, Inc.
+ *
+ *  Created on: Jun 14, 2018
+ *      Author: gdbeckstein
+ */
+
+
+
