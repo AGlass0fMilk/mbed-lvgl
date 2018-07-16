9d6db35277e796770569b6204ca692395d551d02
aglass0fmilk <donbeckstein@live.com>
2018-07-16 13:11:48 -0400

    Reworking the DisplayDriver abstraction structure

---
+++ drivers/DisplayInterface.h
@@ -0,0 +1,29 @@
+/*
+ * DisplayInterface.h
+ *
+ * Interface for communicating with a display
+ *
+ * Allows the underlying transport (Parallel, I2C, SPI, etc)
+ * to be changed without modifying the behavior of the
+ * DisplayDriver using the interface
+ *
+ *  Created on: Jul 16, 2018
+ *      Author: gdbeckstein
+ */
+
+#ifndef DRIVERS_DISPLAYINTERFACE_H_
+#define DRIVERS_DISPLAYINTERFACE_H_
+
+class DisplayInterface
+{
+	virtual ~DisplayInterface(void) { }
+
+	virtual void write_command(uint8_t command) = 0;
+
+	virtual void write_data(uint8_t* data, uint32_t length) = 0;
+
+	virtual uint8_t read(void) = 0;
+
+};
+
+#endif /* DRIVERS_DISPLAYINTERFACE_H_ */
