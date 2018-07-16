8f00745719ee7521cbb8b8bdc397d4045df7593d
aglass0fmilk <donbeckstein@live.com>
2018-07-16 12:39:37 -0400

    Adding Parallel8Bit driver so the HX8357D driver can use SPI/I2C/Parallel depending on the interface passed to its constructor

---
+++ drivers/Parallel8Bit.cpp
@@ -0,0 +1,17 @@
+/*
+ * Parallel8Bit.cpp
+ *
+ *  Created on: Jul 16, 2018
+ *      Author: gdbeckstein
+ */
+
+#include "Parallel8Bit.h"
+
+#if DEVICE_PARALLEL_8BIT
+
+
+
+
+#endif /* DEVICE_PARALLEL_8BIT
+
+
---
+++ drivers/Parallel8Bit.h
@@ -0,0 +1,16 @@
+/*
+ * Parallel8Bit.h
+ *
+ *  Created on: Jul 16, 2018
+ *      Author: gdbeckstein
+ */
+
+#ifndef DRIVERS_PARALLEL8BIT_H_
+#define DRIVERS_PARALLEL8BIT_H_
+
+#if DEVICE_PARALLEL_8BIT
+
+
+#endif /* DEVICE_PARALLEL_8BIT
+
+#endif /* DRIVERS_PARALLEL8BIT_H_ */
